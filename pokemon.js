3e5bc4d5cda6edea484bb7238a43fdf27c4ba065
NathanRodriguez04 <naterod26@gmail.com>
2021-08-10 09:24:50 -0500

    pokemon

---
+++ pokemon.js
@@ -0,0 +1,27 @@
+//make sure to put all your code in functions
+
+function onClick() {
+    $("#catch-button").click(function (e) {
+        fetch("https://pokeapi.co/api/v2/pokemon", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+        }).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+            randomizePokemon(data)
+        });
+    })
+}
+
+function randomizePokemon(pokemon){
+    console.log(pokemon.results)
+    // get data that was passed in from on Click function
+
+//    randomize one pokemon from array (pokemon.results)
+}
+
+onClick();
